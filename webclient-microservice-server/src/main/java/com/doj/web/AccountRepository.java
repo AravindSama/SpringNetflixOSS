cdb9179bf42ce7a6b0dc92b28d983baf450d33d1
AravindSama <aravind.sama12@gmail.com>
2017-11-16 11:04:52 -0600

    SpringNetflix

---
+++ webclient-microservice-server/src/main/java/com/doj/web/AccountRepository.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.doj.web;
+
+import java.util.List;
+
+
+public interface AccountRepository {
+	
+	List<Account> getAllAccounts();
+	
+	Account getAccount(String number);
+}
